0a069e3823196caf9f71f1118f20dba4e1f5c492
HatounAhmad <hatoun.aldahami@gmail.com>
2026-03-12 04:54:19 +0300

    configure port for Render deployment

--- Program.cs
+++ Program.cs
@@ -1,16 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Use Render's PORT environment variable
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5200";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
-
 app.UseDefaultFiles();
 app.UseStaticFiles();
-
 app.UseAuthorization();
 app.MapControllers();
 
